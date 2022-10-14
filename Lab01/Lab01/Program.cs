e0760d9d1566c87f9da5d5b8da8544496d4bd8d3
Daniel Józefiuk <daniel.jozefiuk@bcoders.pl>
2022-10-14 13:18:53 +0200

    Added lab01

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,40 @@
+﻿try
+{
+    var commandLineArgs = Environment.GetCommandLineArgs();
+    if (commandLineArgs.Length == 1) throw new ArgumentException("Missing all arguments");
+    if(commandLineArgs.Length == 2 && commandLineArgs[1] == "help")
+    {
+        ShowHelp();
+        return;
+    }
+    if (commandLineArgs.Length == 2) throw new ArgumentException("Missing 2nd and 3rd argument");
+    if (commandLineArgs.Length == 3) throw new ArgumentException("Missing 3rd argument");
+    var num1 = float.Parse(commandLineArgs[1]);
+    var operation = commandLineArgs[2];
+    var num2 = float.Parse(commandLineArgs[3]);
+    if (num2 == 0 && operation == "/") throw new InvalidOperationException("Cannot divide by 0");
+    switch (operation)
+    {
+        case "+": Console.WriteLine($"{num1} {operation} {num2} = {num1 + num2}"); return;
+        case "-": Console.WriteLine($"{num1} {operation} {num2} = {num1 - num2}"); return;
+        case "*": Console.WriteLine($"{num1} {operation} {num2} = {num1 * num2}"); return;
+        case "/": Console.WriteLine($"{num1} {operation} {num2} = {num1 / num2}"); return;
+        default: throw new InvalidOperationException($"Operation '{operation} is not supported");
+    }
+} 
+catch(Exception ex)
+{
+    Console.WriteLine("ERROR");
+    Console.WriteLine(ex.Message);
+    Console.WriteLine();
+    ShowHelp();
+}
+
+static void ShowHelp()
+{
+    Console.WriteLine("Command line arguments:");
+    Console.WriteLine("arg1 arg2 arg3");
+    Console.WriteLine("arg1 - first number");
+    Console.WriteLine("arg2 - operation ( + - / * )");
+    Console.WriteLine("arg3 - second number");
+}
---
+++ Lab01/Zad02/Program.cs
@@ -0,0 +1,53 @@
+﻿while(true)
+{
+    ShowMenu();
+    var input = Console.ReadKey().KeyChar;
+    if (input == '0') return;
+    var keys = ReadKeys();
+    switch(input)
+    {
+        case '1': Console.WriteLine($"{keys.Item1} + {keys.Item2} = {keys.Item1 + keys.Item2}"); break;
+        case '2': Console.WriteLine($"{keys.Item1} - {keys.Item2} = {keys.Item1 - keys.Item2}"); break;
+        case '3': Console.WriteLine($"{keys.Item1} * {keys.Item2} = {keys.Item1 * keys.Item2}"); break;
+        case '4': 
+            if(keys.Item2 == 0)
+            {
+                Console.WriteLine("NEVER divide by 0.");
+                break;
+            }
+            Console.WriteLine($"{keys.Item1} / {keys.Item2} = {keys.Item1 / keys.Item2}"); 
+            break;
+    }
+}
+
+static void ShowMenu()
+{
+    Console.Write(@"
+[1] +
+[2] -
+[3] *
+[4] /
+[0] Exit
+");
+}
+
+static Tuple<float,float> ReadKeys()
+{
+    string? str1, str2;
+    float _;
+    do
+    {
+        Console.Write("\nFirst number: ");
+        str1 = Console.ReadLine();
+    } while (str1 == null || str1.Length == 0 || float.TryParse(str1, out _) is false);
+    float n1 = float.Parse(str1);
+
+    do
+    {
+        Console.Write("\nSecond number: ");
+        str2 = Console.ReadLine();
+    } while (str2 == null || str2.Length == 0 || float.TryParse(str2, out _) is false);
+    float n2 = float.Parse(str2);
+
+    return Tuple.Create(n1, n2);
+}
